d00bb4176347833349e01fa7f141abd3aa3e86bf
Matt Holland <matt@mattholland.com>
2017-10-16 19:58:25 -0700

    updating references to api keys

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -18,7 +18,7 @@ donor_url = 'https://actionnetwork.org/api/v2/people/' + params[:person][:action
     method: :post,
     url: campaign_url,
     payload: {"identifiers":[ params[:campaign][:action_network_id]  ],"recipients":[{"display_name": params[:donation][:display_name] ,"amount": params[:donation][:amount] }],"_links":{"osdi:person":{"href":donor_url}}},
-    headers: {'content_type' => :json, 'OSDI-API-Token' => '449ad9a708611ca4c91d99511ea3ff40'}
+    headers: {'content_type' => :json, 'OSDI-API-Token' => @keys['ruckus_api']}
   }).execute do |response, request, result|
     case response.code
     when 400
