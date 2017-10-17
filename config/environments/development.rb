167c7d86a34681fcc94992ee3dae8b9f3843306d
Matt Holland <matt@mattholland.com>
2017-10-16 17:13:10 -0700

    changes to remove need for a database
    from https://revelry.co/making-a-rails-app-without-a-database/

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,8 +38,9 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  # removed by Matt H. to deal with running without a database
   # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
+  # config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
