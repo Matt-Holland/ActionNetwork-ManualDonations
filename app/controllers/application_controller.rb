19ecff5ac0d50f7dc86703eed5259d94762a6b00
Matt Holland <matt@mattholland.com>
2017-10-27 12:45:57 -0700

    corrected silly typo that was breaking authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['RUCKUS_USER'] && password == ENV['RUCKUS_PASS']
+      username = ENV['RUCKUS_USER'] && password = ENV['RUCKUS_PASS']
     end
   end
 
